e231d810334a0de6659bf118921c17b77b72009c
mansurjan <mansur.allaberdiyev.98@mail.ru>
2023-04-09 15:25:32 +0500

    leadeboard functionality

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,23 +1,10 @@
-const leaderboard = [
-  { id: 1, username: "user1", level: "easy", score: 10, time: "1:00" },
-  { id: 2, username: "user2", level: "medium", score: 20, time: "1:30" },
-  { id: 3, username: "user3", level: "hard", score: 30, time: "2:00" },
-  { id: 4, username: "user4", level: "easy", score: 8, time: "1:20" },
-  { id: 5, username: "user5", level: "medium", score: 18, time: "2:00" },
-  { id: 6, username: "user6", level: "hard", score: 24, time: "2:30" },
-  { id: 7, username: "user7", level: "easy", score: 6, time: "1:30" },
-  { id: 8, username: "user8", level: "medium", score: 16, time: "1:45" },
-  { id: 9, username: "user9", level: "hard", score: 18, time: "3:00" },
-];
+import { getRatingsByLevel } from "./test.js";
 
 export const showFilteredLeaderboard = (level) => {
   // Get the leaderboard from local storage
-  //   const leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
-
+  const ratings = getRatingsByLevel(level);
   // Filter the leaderboard to only show entries with the specified level
-  const filteredLeaderboard = leaderboard.filter(
-    (entry) => entry.level === level
-  );
+  const filteredLeaderboard = ratings.filter((entry) => entry.level === level);
 
   // Sort the filtered leaderboard by score in descending order
   filteredLeaderboard.sort((a, b) => b.score - a.score);
@@ -36,15 +23,15 @@ export const showFilteredLeaderboard = (level) => {
   const rankHeader = headerRow.insertCell();
   const usernameHeader = headerRow.insertCell();
   const scoreHeader = headerRow.insertCell();
-  const timeHeader = headerRow.insertCell();
+  const dateHeader = headerRow.insertCell();
   rankHeader.innerHTML = "Rank";
   rankHeader.classList.add("column__header");
   usernameHeader.innerHTML = "Username";
   usernameHeader.classList.add("column__header");
   scoreHeader.innerHTML = "Score";
   scoreHeader.classList.add("column__header");
-  timeHeader.innerHTML = "Time";
-  timeHeader.classList.add("column__header");
+  dateHeader.innerHTML = "Date";
+  dateHeader.classList.add("column__header");
 
   // Add a row for each leaderboard entry
   filteredLeaderboard.forEach((entry, index) => {
@@ -64,9 +51,14 @@ export const showFilteredLeaderboard = (level) => {
     scoreCell.innerHTML = entry.score;
     scoreCell.classList.add("cell");
     // Add the time column
-    const timeCell = row.insertCell();
-    timeCell.innerHTML = entry.time;
-    timeCell.classList.add("cell");
+    const dateCell = row.insertCell();
+
+    const date = `${new Date(entry.time).getDate()}/${new Date(
+      entry.time
+    ).getMonth()}/${new Date(entry.time).getFullYear()}`;
+
+    dateCell.innerHTML = date;
+    dateCell.classList.add("cell");
   });
 };
 
