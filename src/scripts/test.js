b283ce426af10d08b811b7a2768c6b62e0344d11
davron129 <normominovdavron001@gmail.com>
2023-04-09 00:33:24 -0700

    feat: auth functions added

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,11 +1,47 @@
 'use strict'
 import {data} from "../store/db.js"
 
+const RATINGS = 'ratings';
+const USERS = 'users';
+const CUR_USER = 'curUser';
+
+/**
+ * 
+ * @param {string} field 
+ * @returns {Array}
+ */
+function get(field) {
+    const result = localStorage.getItem(field);
+
+    if(result) {
+        return JSON.parse(result);
+    }
+
+    return [];
+}
+
+/**
+ * 
+ * @param {string} field 
+ * @param {string} value 
+ */
+function set(field, value) {
+    localStorage.setItem(field, JSON.stringify(value));
+}
+
+/**
+ * 
+ * @param {string} field 
+ */
+function remove(field) {
+    localStorage.removeItem(field);
+}
+
 /**
  * 
  * @param  {"easy" | "medium" | "hard"} level 
  */
-function getWordsByLevel(level) {
+export function getWordsByLevel(level) {
     const questions = data.filter((question) => question.level === level);
     const questionsAmount = questions.length;
 
@@ -30,7 +66,7 @@ function getWordsByLevel(level) {
  * @param {"easy" | "medium" | "hard"} level 
  * @param {number} score 
  */
-function addToRating(username, level, score) {
+export function addToRating(username, level, score) {
     const rating = {
         id: Date.now(),
         username,
@@ -39,26 +75,67 @@ function addToRating(username, level, score) {
         time: Date.now()
     }
 
-    let ratings = JSON.parse(localStorage.getItem("ratings"));
+    let ratings = get(RATINGS);
 
     if(!ratings) {
-        localStorage.setItem("ratings", JSON.stringify([]));
+        set(RATINGS, ratings);
         ratings = [];
     }
 
     ratings.push(rating);
 
-    localStorage.setItem('ratings', JSON.stringify(ratings));
+    set(RATINGS, ratings);
 }
 
 /**
  * 
  * @param {"easy" | "medium" | "hard"} level 
  */
-function getRatingsByLevel(level) {
-    const ratings = localStorage.getItem("ratings") ?? [];
+export function getRatingsByLevel(level) {
+    const ratings = get(RATINGS);
 
     return ratings.filter((rating) => rating.level === level)
+}
+
+/**
+ * 
+ * @param {string} username 
+ * @param {string} password 
+ * @returns 
+ */
+export function handleLogin(username, password) {
+    const users = get(USERS);
+
+    const filteredUser = users.filter((user) => user.username === username && user.password === password);
+
+    const isExists = filteredUser.length > 0;
+
+    if(isExists) {
+        const curUser = filteredUser[0];
+        set(CUR_USER, curUser);
+    }
+
+    return isExists;
+}
+
+export function handleRegister(name, username, password) {
+    const isExists = handleLogin(username, password);
+
+    if(!isExists) {
+        return false;
+    }
+
+    const curUser = {
+        name,
+        username,
+        password
+    }
+
+    set(CUR_USER, curUser);
+}
+
+export function handleLogout(field) {
+    localStorage.removeItem(field);
 }
 
 const button = document.getElementById("button");
